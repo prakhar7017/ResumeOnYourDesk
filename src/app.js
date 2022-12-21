93645ebf395833b5d04a9070e7180e0d7fc1b6b8
Prakhar Maheshwari <prakhar2115430@akgec.ac.in>
2022-12-22 04:26:06 +0530

    solved error

--- src/app.js
+++ src/app.js
@@ -85,14 +85,14 @@ app.get("/resume/:theme",loggedIn,(req,res,next)=>{
   }
 
 });
-
+"index.html", "utf8"
 
 app.post("/resume-maker",loggedIn,(req,res,next)=>{
   let theme_used=req.body.theme;
   let data=req.body;
   if(theme_used==="1"){
     const filename=Math.random()+"_doc"+".pdf";
-    var html = fs.readFileSync("index.html", "utf8");
+    var html = fs.readFileSync(path.join(__dirname,"./views/index.html"),"utf-8");
     var options = {
       format: "A3",
       orientation: "portrait",
@@ -150,7 +150,7 @@ app.post("/resume-maker",loggedIn,(req,res,next)=>{
 })
   }else{
     const filename=Math.random()+"_doc"+".pdf";
-    var html = fs.readFileSync("index2.html", "utf8");
+    var html = fs.readFileSync(path.join(__dirname,"./views/index.html"),"utf-8");
     var options = {
       format: "A3",
       orientation: "portrait",
